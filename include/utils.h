c51a38cd9d9ee21ed18a2fd191bf452b66adf41b
Harsh <harsh97830544@gmail.com>
2025-11-27 19:15:48 +0530

    Enhance clerscrn function documentation
    Added comments to clarify the purpose of clerscrn function.

--- include/utils.h
+++ include/utils.h
@@ -1,6 +1,8 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-void clerscrn();    // for clear screen 
+// This function clears the console screen
+// It is used after each menu action for better display
+void clerscrn();
 
 #endif
