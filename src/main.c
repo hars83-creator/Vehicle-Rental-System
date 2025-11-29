9f9946a19b3c0f9716d82e13eb2a17184d0a7e0e
Harsh <harsh97830544@gmail.com>
2025-11-29 07:38:08 +0000

    update speeling mistake in savefile

--- src/main.c
+++ src/main.c
@@ -31,7 +31,7 @@ int main() {
                 break;
 
             case 3:
-                saveFIle();    /* Save data to file */
+                saveFile();    /* Save data to file */
                 break;
 
             case 0:
